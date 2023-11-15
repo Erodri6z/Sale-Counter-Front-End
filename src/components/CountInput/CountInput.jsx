5d837ac6531c6047c335eb4ab458356529c8e843
Erodri6z <erikrodriguez2417@gmail.com>
2023-11-15 10:13:07 -0600

    stub up the input forms for the count and price updates

---
+++ src/components/CountInput/CountInput.jsx
@@ -0,0 +1,16 @@
+const CountInput = () => {
+  return (
+    <div>
+      <form >
+        <label>How many did you get?</label>
+        <input 
+        type="number"
+        name="#"
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default CountInput
---
+++ src/components/PriceCount/PriceInput.jsx
@@ -0,0 +1,16 @@
+const PriceInput = () => {
+  return (
+    <div>
+      <form >
+        <label>How Much?</label>
+        <input 
+        type="number"
+        name="#"
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default PriceInput
