38c5c50df1b7262255413a4fcc3de1f9dff5aea5
Erodri6z <erikrodriguez2417@gmail.com>
2023-11-15 12:50:11 -0600

    impliment the form change depending on the selected button

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -14,24 +14,40 @@ const Landing = ({ user }) => {
   }
   console.log(form)
 
+
+
   return (
     <main className={styles.container}>
       <h1>hello, {user ? user.userName : 'friend'}</h1>
-      <div className='count-input insurance'>
-      <CountInput title='Insurance count' /> 
-      </div>
-      <div className='count-input apple'>
-      <CountInput title='Apple-Care count' /> 
-      </div>
-      <div className='count-input prepaid'>
-      <CountInput title='Prepaid Phone count'/> 
-      </div>
-      <div className='count-input accesories'>
-      <PriceInput title='Accesories Price Count' />
-      </div>
-      <div className='count-input gen-electrics'>
-      <PriceInput title='General Electronics Price count' />
-      </div>
+      {
+        (form === 'insurance')?
+        <div className='count-input insurance'>
+          <CountInput title='Insurance count' /> 
+        </div>
+        :
+        (form === 'apple')?
+        <div className='count-input apple'>
+          <CountInput title='Apple-Care count' /> 
+        </div>
+        :
+        (form === 'prepaid')?
+        <div className='count-input prepaid'>
+          <CountInput title='Prepaid Phone count'/> 
+        </div>
+        :
+        (form === 'access')?
+        <div className='count-input accesories'>
+          <PriceInput title='Accesories Price Count' />
+        </div>
+        :
+        (form === 'gen-elec')?
+        <div className='count-input gen-electrics'>
+          <PriceInput title='General Electronics Price count' />
+        </div>
+        :
+        <span></span>
+      }
+
       <Buttons onUpdateForm={updateForm} />
     </main>
   )
