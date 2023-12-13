4c72be64bb0c3f28af05504b1a2706ce25fe813a
Erodri6z <erikrodriguez2417@gmail.com>
2023-12-13 12:28:06 -0600

    add results page

---
+++ src/pages/ResultsPage/ResultsPage.jsx
@@ -0,0 +1,15 @@
+const ResultsPage = () => {
+  return (
+    <div>
+      <h1>This is where the results will be</h1>
+      <div>
+        <p>Insurance</p>
+        <p>AppleCare</p>
+        <p>Accesories</p>
+        <p>General Electronics</p>
+      </div>
+    </div>
+  )
+}
+
+export default ResultsPage
