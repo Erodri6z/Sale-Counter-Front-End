e1817a531c5e6f0a8411c250438ee4cdb072db7f
Erodri6z <erikrodriguez2417@gmail.com>
2023-12-14 11:53:00 -0600

    add the results to the results page and add copy function

--- src/pages/ResultsPage/ResultsPage.jsx
+++ src/pages/ResultsPage/ResultsPage.jsx
@@ -1,19 +1,33 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 const ResultsPage = ({ profile }) => {
-  // const data = profile
-  // const { profile } = location.state || 0
+
   if (!profile) return (<h1>Loading...</h1>)
   const data = profile.sales[0]
+
+  const copyText = () => {
+    navigator.clipboard.writeText(
+      `All-State Protection Sold: ${data.insuranceCount},
+Apple-Care Sold: ${data.appleCareCount},
+Accessories: $${data.accesoriesDollarAmount}.
+General Electronics: $${data.generalElectronicsDollarAmount}`
+    )
+  }
+  
   return (
     <>
     <div>
       <h1>This is where the results will be</h1>
+      <Link to="/">
+        <button>Back to Counter?</button>
+      </Link>
       <div>
         <p>Insurance Sold: {data.insuranceCount}</p>
         <p>AppleCare Sold: {data.appleCareCount}</p>
         <p>Accessories: ${data.accesoriesDollarAmount}</p>
         <p>General Electronics: ${data.generalElectronicsDollarAmount}</p>
+        <button onClick={copyText}>Copy</button>
       </div>
     </div>
     </>
