d2c9937e8eef6486d7c6e2681eeaee37111a3411
Erodri6z <erikrodriguez2417@gmail.com>
2023-12-16 20:39:52 -0600

    add the foundation to the are you sure pop up

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -15,6 +15,7 @@ const Landing = ({ user, profile, handleUpdateCount, createCounter, clearCounter
 
 
   const [form, setForm] = useState('Nothing selected')
+  const [isOpen, setIsOpen] = useState(false)
 
   const updateForm = (newValue) => {
     setForm(newValue)
@@ -28,6 +29,15 @@ const Landing = ({ user, profile, handleUpdateCount, createCounter, clearCounter
     }
   }
 
+  const openMessage = () => {
+  setIsOpen(true)
+  }
+
+  const closeMessage = () => {
+    setIsOpen(false)
+  }
+
+
   if (!profile) return (<h1>Loading...</h1>)
 
   // console.log(profile)
@@ -35,14 +45,23 @@ const Landing = ({ user, profile, handleUpdateCount, createCounter, clearCounter
 
   return (
     <main className="container">
-      <Link to="/results" state={{ profile }}>
-        <button className='results-btn'>Results</button>
+      <Link to="/results" className='results-a' state={{ profile }}>
+        <button className='results-btn' >
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-pie-chart-fill" viewBox="0 0 16 16">
+            <path d="M15.985 8.5H8.207l-5.5 5.5a8 8 0 0 0 13.277-5.5zM2 13.292A8 8 0 0 1 7.5.015v7.778l-5.5 5.5zM8.5.015V7.5h7.485A8.001 8.001 0 0 0 8.5.015z"/>
+          </svg>
+        </button>
       </Link>
       <h1>Hello, {user ? user.userName : 'friend'}</h1>
       {
         profile.sales.length?(
           <>
-          <button onClick={() => reset()} className='reset-btn'>Reset</button>
+          <button onClick={openMessage} className='reset-btn'>Reset</button>
+          <div className={`custom-alert ${isOpen ? 'open' : ''}`}>
+            <div className="alert-content">
+              <button onClick={closeMessage}>Close</button>
+            </div>
+          </div>
         {
           (form === 'insurance')?
           <div className='count-input insurance'>
