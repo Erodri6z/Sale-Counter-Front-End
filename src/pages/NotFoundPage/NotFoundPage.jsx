6753316e26862e9441f4d60b552379806d6c3f67
Erodri6z <erikrodriguez2417@gmail.com>
2024-04-07 11:54:03 -0500

    add a link to the 404 page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -5,7 +5,9 @@ const NotFound = () => {
   <div style={{ textAlign: 'center', marginTop: '50px' }}>
     <h1>( 404 )</h1>
     <h3>You are not suppose to be here!</h3>
-    <h4>Click here to go back</h4>
+    <a href="/" >
+      <h4>Click here to go back</h4>
+    </a>
   </div>
   )
 
